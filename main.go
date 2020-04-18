e0004daf549b1f385be7dccc6726b2cf0f55ae20
Al Sharairi <asharairi@gmail.com>
2020-04-17 19:27:43 -0500

    add parseStack struct

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
+import "container/list"
+
 ////////////////////////////////////////////////////////////////
 
+/********* Parse Stack Item *********/
 type parseStackItem struct {
 	grammarSymbol, stateSymbol string
 }
@@ -9,6 +12,24 @@ type parseStackItem struct {
 func (item parseStackItem) String()  string {
 	return item.grammarSymbol + item.stateSymbol
 }
+
+
+/********* Parse Stack *********/
+
+// using a struct w/ one field to help simulate call by ref
+// needed to obtain the "stack" side-effects w/ push/pop
+type parseStack struct {
+	stack *list.List
+}
+
+func newParseStack() parseStack {
+	pStack := parseStack{}
+	pStack.stack = list.New()
+	return pStack
+}
+
+
+
 
 func main() {
 	//reader := bufio.NewScanner(os.Stdin)
