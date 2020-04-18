fe0039d6db38e3a9ceb4936d3ed668cb4a4e55e7
Al Sharairi <asharairi@gmail.com>
2020-04-17 19:19:10 -0500

    comment file io
    seems like we'll only being taking input from a single hardcoded source. sec 7 of the project pdf

--- main.go
+++ main.go
@@ -1,37 +1,35 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
-)
-
 ////////////////////////////////////////////////////////////////
 
+
+
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-	fmt.Println("Enter a file to parse. Must be in this directory.")
-	file := ""
-	for reader.Scan() {
-		if _, err := os.Stat(reader.Text()); !os.IsNotExist(err) {
-			file = reader.Text()
-			break
-		} else {
-			fmt.Println("File not in directory")
-		}
-	}
+	//reader := bufio.NewScanner(os.Stdin)
+	//fmt.Println("Enter a file to parse. Must be in this directory.")
+	//file := ""
+	//for reader.Scan() {
+	//	if _, err := os.Stat(reader.Text()); !os.IsNotExist(err) {
+	//		file = reader.Text()
+	//		break
+	//	} else {
+	//		fmt.Println("File not in directory")
+	//	}
+	//}
+	//
+	//filepath := file
+	//
+	//filebuffer, err := ioutil.ReadFile(filepath)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	os.Exit(1)
+	//}
+	//
+	//inputdata := string(filebuffer)
+	//data := bufio.NewScanner(strings.NewReader(inputdata))
+	//data.Split(bufio.ScanRunes)
 
-	filepath := file
+	inputArray := []string{"id", "+", "id", "*", "id"}
 
-	filebuffer, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
-	inputdata := string(filebuffer)
-	data := bufio.NewScanner(strings.NewReader(inputdata))
-	data.Split(bufio.ScanRunes)
 }
